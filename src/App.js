5dd9efd7277f103387230736c2617dc08b80a3a4
nahid7g <nahid.h.kiran@gmail.com>
2023-02-07 11:41:45 +0600

    Icon added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import router from './Routes/Routes'
 import './App.css'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { getAuth } from 'firebase/auth'
+import app from './firebase/firebase.init'
+
+const auth = getAuth(app)
 
 function App() {
   return (
